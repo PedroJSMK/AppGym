5349c3e3891d8b374d5816fa6813266c3d9d7e06
PJSMK <pedrokanagusto@hotmail.com>
2022-02-05 07:37:58 -0300

    Initial Commit

---
+++ AppGym/AppGymApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppGymApp.swift
+//  AppGym
+//
+//  Created by Pedro Kanagusto on 05/02/22.
+//
+
+import SwiftUI
+
+@main
+struct AppGymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
